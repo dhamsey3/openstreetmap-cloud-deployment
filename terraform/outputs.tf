a61aed993c69c8cb374a10d8a23a8d3001864253
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-05-19 22:57:45 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,16 @@ output "s3_bucket_name" {
 output "ec2_public_ip" {
   value = aws_instance.web.public_ip
 }
+
+# Outputs for secret ARNs
+output "db_password_arn" {
+  value = aws_secretsmanager_secret.db_password.arn
+}
+
+output "db_username_arn" {
+  value = aws_secretsmanager_secret.db_username.arn
+}
+
+output "db_name_arn" {
+  value = aws_secretsmanager_secret.db_name.arn
+}
