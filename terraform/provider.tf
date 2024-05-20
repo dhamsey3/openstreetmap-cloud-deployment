0451f51cf7a7ea695da45a891b77aa41b7511520
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-05-20 17:40:02 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,3 +12,14 @@ provider "aws" {
     }
   }
 }
+
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
